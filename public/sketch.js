4fafaead94a119767baa55636e02f337c8d1d977
mikima <mikimauri@gmail.com>
2021-11-17 12:55:17 +0100

    added broadcast

--- public/sketch.js
+++ public/sketch.js
@@ -1,16 +1,33 @@
 let clientSocket = io();
 
 clientSocket.on("connect", newConnection);
+clientSocket.on("mouseBroadcast", newBroadcast);
 
 function newConnection() {
   console.log(clientSocket.id);
 }
 
+function newBroadcast(data) {
+  console.log(data);
+  fill("red");
+  circle(data.x, data.y, 10);
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  background(220);
 }
 
 function draw() {
-  background(220);
+  fill("yellow");
   circle(mouseX, mouseY, 20);
+}
+
+function mouseMoved() {
+  let message = {
+    x: mouseX,
+    y: mouseY,
+  };
+
+  clientSocket.emit("mouse", message);
 }
