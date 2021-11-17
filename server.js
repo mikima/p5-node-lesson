ff96b4798f043a10c8dbff4094dd280c4d045db2
mikima <mikimauri@gmail.com>
2021-11-17 11:33:40 +0100

    added sketch

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 console.log("up and running");
+
+let express = require("express");
+
+let app = express();
+
+let port = 3000;
+
+let server = app.listen(port);
+
+console.log("Server is raunning on http://localhost:" + port);
+
+app.use(express.static("public"));
